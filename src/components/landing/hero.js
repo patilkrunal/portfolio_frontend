55baba88a46a1771753fdcbedceab6c3fccbd49d
patilkrunal <patilkrunal11.kp@gmail.com>
2021-03-12 17:37:51 +0530

    First commit full website basic done

---
+++ src/components/landing/hero.js
@@ -0,0 +1,85 @@
+import React from "react"
+
+import Image from "../image"
+import Links from "../links"
+
+import { Link } from "react-scroll"
+
+export default function Hero() {
+  return (
+    <section className="hero" id="hero">
+      <div className="container h-100">
+        <div className="row h-100 justify-content-around">
+          {/* + Hero section: Left side */}
+          <div className="col-lg-6 my-auto">
+            <div className="hero-introduction">
+              <h1>
+                <span className="wave" role="img" aria-label="Hand wave">
+                  👋
+                </span>
+              </h1>
+              <h1>Hi. I'm Krunal</h1>
+              <p>
+                I am a Computer Engineer at Army Institute of Technology, Pune. 
+                Let me know how you like my portfolio?
+              </p>
+              <Link
+                to="content"
+                className="main-button btn btn-primary shadow-sm"
+                smooth={true}
+              >
+                Get to know me
+              </Link>
+              <a
+                href="https://drive.google.com/file/d/1LZzIGyWxyKUhUsCgyEPgHvxJL9BkH9P7/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+                className="main-button btn btn-primary shadow-sm ml-3"
+              >
+                Download my resume
+              </a>
+            </div>
+          </div>
+          {/* - Hero section: Left side */}
+
+          {/* + Hero section: Right side */}
+          <div className="col-lg-4 my-auto">
+            <div className="hero-bio">
+              <div className="card shadow-sm mb-5">
+                <div className="card-body">
+                  <div className="icon">
+                    <Image
+                      fileName="photo.jpg"
+                      style={{ width: "5rem", height: "5rem" }}
+                      alt="Hero Icon"
+                    />
+                  </div>
+                  <h3 className="card-title">@patilkrunal</h3>
+                  <h4>
+                    <span
+                      className="mr-1"
+                      role="img"
+                      aria-label="pin"
+                      alt="pin emoji"
+                    >
+                      📍
+                    </span>
+                    Nashik
+                    <br />
+                  </h4>
+                  <p className="card-text">
+                    Backend enthusiast,
+                    <br />
+                    Compassionate
+                  </p>
+                  <Links />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* - Hero section: Right side */}
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/components/links.js
@@ -0,0 +1,56 @@
+import React from "react"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import {
+  faTwitter,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons"
+
+export default function Links() {
+  return (
+    <ul className="list-group list-group-horizontal">
+      <li className="list-group-item">
+        <a
+          href="mailto:patilkrunal11.kp@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          alt="email link"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </li>
+      <li className="list-group-item">
+        <a
+          href="https://github.com/patilkrunal"
+          target="_blank"
+          rel="noreferrer"
+          alt="github link"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </li>
+      <li className="list-group-item">
+        <a
+          href="https://linkedin.com/patilkrunal"
+          target="_blank"
+          rel="noreferrer"
+          alt="linkedin link"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </li>
+      <li className="list-group-item">
+        <a
+          href="https://twitter.com/KP_11_"
+          target="_blank"
+          rel="noreferrer"
+          alt="twitter link"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </li>
+    </ul>
+  )
+}
